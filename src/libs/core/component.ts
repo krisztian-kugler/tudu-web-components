331d499de9be6aef7a5512649025cd9a67227ed7
Krisztian Kugler <krisztian.kugler@gmail.com>
2020-04-06 21:05:54 +0200

    refactored component decorator

--- src/libs/core/component.ts
+++ src/libs/core/component.ts
@@ -1,42 +1,25 @@
+import { Injector } from "./injector";
+
 interface ComponentConfig {
   selector: string;
-  providers?: any[];
+  providers?: FunctionConstructor[];
 }
 
 export function Component(config: ComponentConfig) {
   const { selector, providers = [] } = config;
 
-  if (!selector.includes("-")) throw new Error(`'${selector}' is not a valid selector. Please include a dash.`);
+  if (!selector.includes("-")) throw new Error(`'${selector}' is not a valid selector. A dash must be included.`);
 
   return function <T extends { new (...args: any[]): any }>(constructor: T) {
-    constructor.prototype.selector = selector;
-    constructor.prototype.providers = {};
-
-    /* providers.forEach((Provider: FunctionConstructor) => {
-      constructor.prototype.providers[Provider.name] = new Provider();
-    }); */
-
-    const decoratedComponent = class extends constructor {
-      providers: { [key: string]: any } = {};
-      selector = selector;
+    return class extends constructor {
+      static selector = selector;
 
       constructor(...args: any[]) {
-        super(...args);
-        providers.forEach((Provider: FunctionConstructor) => {
-          if (!this.providers) this.providers = {};
-          this.providers[Provider.name] = new Provider();
-        });
-      }
-
-      connectedCallback() {
-        super.connectedCallback();
+        super();
+        if (providers.length) Injector.register(this as any, ...providers);
+        this.onInit();
+        this.render();
       }
     };
-
-    decoratedComponent.prototype.selector = selector;
-
-    // customElements.define(selector, decoratedComponent);
-
-    return decoratedComponent;
   };
 }
