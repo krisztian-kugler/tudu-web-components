66b77cf6eb91e15cac8c552a926ef01fe07ad921
Krisztian Kugler <krisztian.kugler@gmail.com>
2020-05-02 13:23:27 +0200

    board and column template/styles updated

--- src/app/components/board/column/column.component.ts
+++ src/app/components/board/column/column.component.ts
@@ -1,12 +1,14 @@
-import { Component } from "@core";
+import { Component, Injector } from "@core";
 import "./column.component.scss";
 import CardComponent from "./card/card.component";
+import { DragDropService } from "../../../services/drag-drop.service";
 
 @Component({
   selector: "tudu-column",
 })
 export default class ColumnComponent extends HTMLElement {
   private _column: any;
+  private dragDropService: DragDropService;
 
   get column(): any {
     return this._column;
@@ -20,18 +22,27 @@ export default class ColumnComponent extends HTMLElement {
     super();
   }
 
-  onInit() {}
+  onInit() {
+    this.dragDropService = Injector.inject(this, DragDropService);
+    this.dragDropService.defineDropzone(this);
+  }
 
   render() {
     this.innerHTML = `
-      <h2 class="column-title">${this.column.title}</h2>
-      <div class="card-list"></div>
+      <div class="column">
+        <h2 class="column-title">${this.column.title}</h2>
+        <div class="card-list"></div>
+        <button class="add-card">Add Card</button>
+      </div>
     `;
 
     for (const issue of this.column.issues) {
+      const cardSlotElement = document.createElement("div");
+      cardSlotElement.classList.add("card-slot");
       const cardElement = document.createElement("tudu-card") as CardComponent;
       cardElement.issue = issue;
-      this.querySelector(".card-list").append(cardElement);
+      cardSlotElement.append(cardElement);
+      this.querySelector(".card-list").append(cardSlotElement);
     }
   }
 }
